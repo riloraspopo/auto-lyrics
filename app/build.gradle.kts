280797bd8d278647da0dd6121d6c1140b85d0c1d
Rilo Raspopo <rilo.raspopo37@gmail.com>
2026-09-07 08:30:48 +0700

    up vert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.autolyrics"
         minSdk = 26
         targetSdk = 34
-        versionCode = 32
-        versionName = "1.9.8-beta"
+        versionCode = 33
+        versionName = "1.9.9-beta"
     }
 
     signingConfigs {
